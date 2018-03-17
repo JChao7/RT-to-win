446b7d44b967c050353642d36a74dcc0fa808387
J_Chao7 <[jonathan.chao7@gmail.com]>
2018-03-17 15:12:10 -0700

    added config and main

---
+++ config.json.py
@@ -0,0 +1,15 @@
+{
+        "search-queries": ["RT to win", "Retweet and win"],
+        "follow-keywords": [" follow ", " follower "],
+        "fav-keywords": [" fav ", " favorite "],
+        "scan-update-time": 1800,
+        "retweet-update-time": 20,
+        "rate-limit-update-time": 10,
+        "min-ratelimit":10,
+        "min-ratelimit-retweet":20,
+        "min-ratelimit-search":40,
+	"consumer-key": "m3eijpF3CWfX2LLGz6QhXsgdn",
+	"consumer-secret": "7ijaNf4YznYfDa3oMXYAFiopHNhEO1C2oMaeca4OEJyoTdd25C",
+	"access-token-key": " 972343757843570688-cHuADxGj6zFZfqltvSWyPsfnQVujokW",
+	"access-token-secret": "3uyiOwxPW9uVas7Pr3p6ePDHH1zsRwB8a9z32OxvaAbnO"
+}
