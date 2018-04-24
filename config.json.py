6ed4284dbe37a509ad992af39248fede3dc4b079
Jonathan Chao <25756956+JChao7@users.noreply.github.com>
2018-04-23 22:56:05 -0700

    Update config.json.py

--- config.json.py
+++ config.json.py
@@ -8,8 +8,8 @@
         "min-ratelimit":10,
         "min-ratelimit-retweet":20,
         "min-ratelimit-search":40,
-	"consumer-key": "1z6XMf00bWf2sgdusrNX582Eh",
-	"consumer-secret": "7wkDdoA7T8hLEkjAhSNbVQEYuvbEVgwc11yRERhky14o7oqNTm",
-	"access-token-key": " 972343757843570688-cHuADxGj6zFZfqltvSWyPsfnQVujokW",
-	"access-token-secret": "3uyiOwxPW9uVas7Pr3p6ePDHH1zsRwB8a9z32OxvaAbnO"
+	"consumer-key": "",
+	"consumer-secret": "",
+	"access-token-key": "",
+	"access-token-secret": ""
 }
